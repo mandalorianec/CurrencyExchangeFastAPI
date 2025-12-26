d176aad27eb32a55878edf5108d16410fc826a7e
mandalorianec <173474864+mandalorianec@users.noreply.github.com>
2025-12-26 23:23:47 +0300

    fix: remove localhost from app.js

--- nginx/frontend/js/app.js
+++ nginx/frontend/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const host = "http://localhost:80"
+    const host = ""
 
     // Fetch the list of currencies and populate the select element
     function requestCurrencies() {
