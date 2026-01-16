8f612bf48aee1d7c0b190f349cfdd4e7e7d81f2e
mandalorianec <173474864+mandalorianec@users.noreply.github.com>
2026-01-16 23:44:51 +0300

    test: add test_convert

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from dishka import Scope, make_async_container, provide
 from dishka.integrations.fastapi import setup_dishka
-from fastapi import Request, Response
+from fastapi import Request, Response, FastAPI
+from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi_limiter.depends import RateLimiter
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy import StaticPool
@@ -9,7 +10,31 @@ from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from app.database import Base
 from app.dependencies import MyProvider
-from main import app
+from app.exception_handler import http_exception_handler, ownexception_handler, validation_exception_handler
+from app.exceptions import BaseOwnException
+from app.routers.currency import currency_router
+from app.routers.exchangerate import exchange_rate_router
+from app.service.exchange_service import ExchangeService
+from app.routers.exchange import exchange_router
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    # Подключаем собственные обработчики ошибок
+    app.add_exception_handler(RequestValidationError, validation_exception_handler)
+    app.add_exception_handler(HTTPException, http_exception_handler)
+    app.add_exception_handler(BaseOwnException, ownexception_handler)
+
+    # Подключаем свои роутеры
+    app.include_router(exchange_router)
+    app.include_router(exchange_rate_router)
+    app.include_router(currency_router)
+    return app
+
+
+@pytest.fixture
+def test_app() -> FastAPI:
+    return create_app()
 
 
 class MockMyProvider(MyProvider):
@@ -19,14 +44,31 @@ class MockMyProvider(MyProvider):
 
 
 @pytest.fixture
-async def client():
+async def client(container, test_app):
+    setup_dishka(container, test_app)
+    client = AsyncClient(transport=ASGITransport(app=test_app), base_url="http://test")
+    yield client
+    await container.close()
+
+
+@pytest.fixture
+async def exchange_service(container, test_app):
+    setup_dishka(container, test_app)
+    async with container() as mini_container:
+        service = await mini_container.get(ExchangeService)
+        yield service
+
+
+@pytest.fixture
+async def container(test_app):
     mock_container = make_async_container(MockMyProvider())
     engine = await mock_container.get(AsyncEngine)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    setup_dishka(mock_container, app)
-    client = AsyncClient(transport=ASGITransport(app=app), base_url="http://test")
-    yield client
+    setup_dishka(mock_container, test_app)
+    yield mock_container
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
     await mock_container.close()
 
 
--- tests/unit_tests/test_service.py
+++ tests/unit_tests/test_service.py
@@ -1,5 +1,9 @@
+from decimal import Decimal
+
 import pytest
 from httpx import AsyncClient
+
+from app.service.exchange_service import ExchangeService
 
 
 @pytest.mark.anyio
@@ -8,3 +12,16 @@ async def test_post_currency(client: AsyncClient):
     response = await client.post("/currencies", data=form_data)
     assert response.status_code == 201
     assert response.json()["code"] == "RUB"
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("from_, to, amount", [("USD", "RUB", Decimal(24))])
+async def test_convert(exchange_service: ExchangeService, from_: str, to: str, amount: Decimal, client: AsyncClient):
+    form_data = {"name": "Russian Ruble", "code": "RUB", "sign": "R"}
+    await client.post("/currencies", data=form_data)
+    form_data = {"name": "US Dollar", "code": "USD", "sign": "$"}
+    await client.post("/currencies", data=form_data)
+    form_data = {"baseCurrencyCode": "USD", "targetCurrencyCode": "RUB", "rate": 23}
+    await client.post("/exchangeRates", data=form_data)
+    response = (await exchange_service.convert(from_, to, amount)).converted_amount
+    assert response == Decimal('552')
