21437fbba253e1f95cbdf4d8cca30d8adb70cd87
mandalorianec <173474864+mandalorianec@users.noreply.github.com>
2026-01-20 23:10:01 +0300

    test: add integration tests

---
+++ tests/integration_tests/test_endpoints.py
@@ -0,0 +1,80 @@
+
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+async def test_get_currency_eur(client: AsyncClient, eur_currency) -> None:
+    response = await client.get(f"/currency/{eur_currency.code}")
+    assert response.status_code == 200
+    assert response.json()["code"] == "EUR"
+
+
+@pytest.mark.anyio
+async def test_post_currency(client: AsyncClient) -> None:
+    form_data = {"name": "Euro", "code": "EUR", "sign": "E"}
+    response = await client.post("/currencies", data=form_data)
+    assert response.status_code == 201
+    assert response.json()["code"] == "EUR"
+
+
+@pytest.mark.anyio
+async def test_get_all_currencies(client: AsyncClient, usd_currency, eur_currency, rub_currency) -> None:
+    response = await client.get(f"/currencies")
+    assert response.status_code == 200
+    assert response.json()[0]['code'] == "USD"
+    assert response.json()[1]['code'] == "EUR"
+    assert response.json()[2]['code'] == "RUB"
+
+
+@pytest.mark.anyio
+async def test_get_all_exchange_rates(client: AsyncClient, exchange_rate_usd_eur, exchange_rate_usd_rub) -> None:
+    response = await client.get("/exchangeRates")
+    assert response.status_code == 200
+    assert response.json()[0]['baseCurrency']['code'] == "USD"
+    assert response.json()[0]['targetCurrency']['code'] == "EUR"
+    assert response.json()[1]['baseCurrency']['code'] == "USD"
+    assert response.json()[1]['targetCurrency']['code'] == "RUB"
+
+
+@pytest.mark.anyio
+async def test_get_exchange_rate_usd_rub(client: AsyncClient, exchange_rate_usd_rub) -> None:
+    response = await client.get("/exchangeRate/USDRUB")
+    assert response.status_code == 200
+    assert response.json()['baseCurrency']['code'] == "USD"
+    assert response.json()['targetCurrency']['code'] == "RUB"
+
+
+@pytest.mark.anyio
+async def test_post_exchange_rate(client: AsyncClient, usd_currency, rub_currency) -> None:
+    form_data = {"baseCurrencyCode": usd_currency.code, "targetCurrencyCode": rub_currency.code, "rate": 120}
+    response = await client.post("/exchangeRates", data=form_data)
+    assert response.status_code == 201
+    assert response.json()['baseCurrency']['code'] == "USD"
+    assert response.json()['targetCurrency']['code'] == "RUB"
+    assert response.json()['rate'] == 120
+
+
+@pytest.mark.anyio
+async def test_patch_exchange_rate(client: AsyncClient, exchange_rate_usd_eur) -> None:
+    form_data = {"rate": 10}
+    response = await client.patch("exchangeRate/USDEUR", data=form_data)
+    assert response.status_code == 200
+    assert response.json()['rate'] == 10
+
+
+@pytest.mark.anyio
+async def test_get_non_existent_exchange_rate(client: AsyncClient) -> None:
+    response = await client.get("/exchangeRate/ABCEDF")
+    assert response.status_code == 404
+    assert "message" in response.json()
+    assert "Обменный курс для пары не найден" in response.json()["message"]
+
+
+@pytest.mark.anyio
+async def test_add_already_existent_currency(client: AsyncClient, usd_currency) -> None:
+    form_data = {"name": "US Dollar", "code": "USD", "sign": "$"}
+    response = await client.post("/currencies", data=form_data)
+    assert response.status_code == 409
+    assert "message" in response.json()
+    assert "Валюта с таким кодом уже существует" in response.json()["message"]
--- tests/unit_tests/test_service.py
+++ tests/unit_tests/test_service.py
@@ -1,39 +1,38 @@
 from decimal import Decimal
 
 import pytest
-from httpx import AsyncClient
 
+from app.exceptions import CurrencyNotFoundError
+from app.service.currency_service import CurrencyService
 from app.service.exchange_service import ExchangeService
-
-
-@pytest.mark.anyio
-async def test_post_currency(client: AsyncClient):
-    form_data = {"name": "Euro", "code": "EUR", "sign": "E"}
-    response = await client.post("/currencies", data=form_data)
-    assert response.status_code == 201
-    assert response.json()["code"] == "EUR"
 
 
 @pytest.mark.anyio
 @pytest.mark.parametrize("amount", [(Decimal(24))])
 async def test_convert_usd_rub(
-        exchange_service: ExchangeService, amount: Decimal, client: AsyncClient, rub_currency, usd_currency,
-        exchange_rate
-):
+        exchange_service: ExchangeService, amount: Decimal, rub_currency, usd_currency,
+        exchange_rate_usd_rub
+) -> None:
     response = (await exchange_service.convert("USD", "RUB", amount)).converted_amount
-    assert response == Decimal(str(exchange_rate.rate)) * amount
+    assert response == Decimal(str(exchange_rate_usd_rub.rate)) * amount
 
 
 @pytest.mark.anyio
 @pytest.mark.parametrize("amount", [(Decimal(24))])
-async def test_convert_rub_usd(exchange_service: ExchangeService, amount: Decimal, client: AsyncClient, rub_currency,
-                               usd_currency, exchange_rate):
+async def test_convert_rub_usd(exchange_service: ExchangeService, amount: Decimal, rub_currency,
+                               usd_currency, exchange_rate_usd_rub) -> None:
     response = (await exchange_service.convert("RUB", "USD", amount)).converted_amount
-    assert response == (1/Decimal(str(exchange_rate.rate)) * amount).quantize(Decimal("0.000001"))
+    assert response == (1 / Decimal(str(exchange_rate_usd_rub.rate)) * amount).quantize(Decimal("0.000001"))
 
 
 @pytest.mark.anyio
-async def test_get_currency(usd_currency, client):
-    response = await client.get(f"/currency/{usd_currency.code}")
-    assert response.status_code == 200
-    assert response.json()["code"] == usd_currency.code
+async def test_convert_eur_rub(exchange_service: ExchangeService, eur_currency, usd_currency, exchange_rate_usd_rub,
+                               exchange_rate_usd_eur) -> None:
+    response = await exchange_service.convert("EUR", "RUB", Decimal('24'))
+    assert response.converted_amount == Decimal('2195.294118')
+
+
+@pytest.mark.anyio
+async def test_get_non_existent_currency(currency_service: CurrencyService) -> None:
+    with pytest.raises(CurrencyNotFoundError):
+        await currency_service.get_currency_by("USD")
