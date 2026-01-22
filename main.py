385fbdd432992369f76c91780e2b6f6091a68cd3
mandalorianec <173474864+mandalorianec@users.noreply.github.com>
2026-01-22 17:48:21 +0300

    feat: add uvicorn via factory

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import logging
 
 import uvicorn
 
-from app.app_factory import create_app
-
 logger = logging.getLogger(__name__)
 
-app = create_app()
-
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.app_factory:create_app", host="0.0.0.0", port=8000, factory=True, reload=True)
